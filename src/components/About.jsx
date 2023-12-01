a6231f57a620bcfdb6e7952719e8bfb6f3b66119
leonelvalentinn <leonelvalentinn@gmail.com>
2023-12-01 13:26:28 -0600

    Cambios

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,7 @@ import "../styles/about.css";
 const About = () => {
   return (
     <div className="about" id="">
-      <div className="container-about">
-        <div className="about-slider" id="aboutSlider">
-          <div className="textos">
-            <h2>Detrás de un gran Odontólogo hay un gran Laboratorio Dental.</h2>
-            
-          </div>
-        </div>
+        
         {/*<button
           className="btn-about"
           id="1"
@@ -26,7 +20,6 @@ const About = () => {
         >
           2
   </button>*/}
-      </div>
     </div>
   );
 };
